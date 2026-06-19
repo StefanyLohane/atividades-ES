7f494482a9566f04cef9663d4515b39f9ad649b5
Stefany_Lohane <202310070022@ifba.edu.br>
2026-06-19 10:49:56 -0300

    Commit revisão

--- src/CadastrarConta.java
+++ src/CadastrarConta.java
@@ -35,5 +35,22 @@ public class CadastrarConta{
         }
         return null;
     }
+    public void adicionarPosicao(Conta conta, int posicao){
+        for(int cont = totalElementos-1; cont>=posicao; cont--){ //cont inicia na primeira posição vazia; enquanto cont estiver perto ou sendo a posição...;diminui de cont para chegar
+            contas[cont+1] = contas[cont]; // lugar q vai copiar o que tá no segundo lugar (ABCC)
+        }
+        contas[posicao] = conta; //substitui o valor que quero na posição q eu quero
+        totalElementos++;
+    }
 
-}
+    public void removerPosicao(Conta conta, int posicao){
+        for(int cont = totalElementos-1; cont>=posicao; cont--){
+            
+        }
+
+
+    }
+
+
+    }
+
